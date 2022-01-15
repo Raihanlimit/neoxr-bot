f66ba8dfb1cb71e61fb20de1fea12a265c621e9b
Raihanlimit <89056745+Raihanlimit@users.noreply.github.com>
2022-01-15 08:37:26 +0700

    Update special-script.js

--- commands/special-script.js
+++ commands/special-script.js
@@ -3,7 +3,7 @@ exports.run = {
 	usage: ['script'],
 	async: async (m, { conn }) => {
 		conn.updatePresence(m.chat, Presence.composing)
-		m.reply(`*Default Source* : https://github.com/neoxr/neoxr-bot`)
+		m.reply(`*Default Source* : SC ? AWOKAWOKAWOK🗿`)
 	},
 	error: false
 }
