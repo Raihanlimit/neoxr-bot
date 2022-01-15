f0ece3b8948fd53474930c89183ad7ab7e55dc46
Raihanlimit <89056745+Raihanlimit@users.noreply.github.com>
2022-01-15 08:35:51 +0700

    Update dl-ig.js

--- commands/dl-ig.js
+++ commands/dl-ig.js
@@ -1,28 +1,23 @@
-let { Presence, MessageType } = require('@adiwajshing/baileys')
-let fetch = require('node-fetch')
 exports.run = {
-	usage: ['ig'],
-	async: async (m, { conn, _func, args, isPrefix, command }) => {
+	usage: ['ig', 'igdl'],
+	async: async (m, { client, args, isPrefix, command }) => {
 	try {
-		await conn.updatePresence(m.chat, Presence.composing)
-		if (!args || !args[0]) return m.reply(`• *Example* : ${isPrefix + command} https://www.instagram.com/p/CK0tLXyAzEI`)
-		if (!args[0].match(/(https:\/\/www.instagram.com)/gi)) return m.reply(_func.status.invalid)
-		m.reply(_func.status.getdata)
-		let json = await (await fetch(global.API('neoxr', '/ig?', { url:https://www.instagram.com/reel/CQngJY4HIhN args[0] }, 'apikey'))).json()
-		if (!json.status) return m.reply(_func.status.fail)
+		if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://www.instagram.com/p/CK0tLXyAzEI'), m)
+		if (!args[0].match(/(https:\/\/www.instagram.com)/gi)) return client.reply(m.chat, global.status.invalid, m)
+		client.reply(m.chat, global.status.getdata, m)
+		let json = await Api.ig(Func.igFixed(args[0]))
+		if (!json.status) return client.reply(m.chat, global.status.fail, m)
 		for(let i = 0; i < json.data.length; i++) {
-			if(json.data[i].type == 'mp4') {
-				await conn.updatePresence(m.chat, Presence.composing) 
-				conn.sendVideo(m.chat, json.data[i].url, null, m)
-			} else if(json.data[i].type == 'jpg') {
-				await conn.updatePresence(m.chat, Presence.composing)
-				conn.sendImage(m.chat, json.data[i].url, null, m)
-			} await _func.delay(1000)
+			if (json.data[i].type == 'mp4') {
+				client.sendVideo(m.chat, json.data[i].url, '', m)
+			} else if (json.data[i].type == 'jpg') {
+				client.sendImage(m.chat, json.data[i].url, '', m)
+			} await Func.delay(1000)
 		}
 	} catch {
-		return m.reply(_func.status.error)
+		return client.reply(m.chat, global.status.error, m)
 	}},
 	error: false,
-	limit: true,
-	premium: false
+	cache: true,
+	location: __filename
 }
